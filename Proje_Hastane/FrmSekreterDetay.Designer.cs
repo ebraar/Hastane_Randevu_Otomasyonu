953a2475923068d74e4096a900423ba795be103a
Ebrar Betul Akgul <ebolakovic@outlook.com>
2023-04-15 18:47:30 +0300

    sekreter kısmını bitirdim. randevu alma işlemlerini veri tabanına bağladım.

--- Proje_Hastane/FrmSekreterDetay.Designer.cs
+++ Proje_Hastane/FrmSekreterDetay.Designer.cs
@@ -188,6 +188,7 @@
             this.BtnKaydet.TabIndex = 14;
             this.BtnKaydet.Text = "Kaydet";
             this.BtnKaydet.UseVisualStyleBackColor = true;
+            this.BtnKaydet.Click += new System.EventHandler(this.BtnKaydet_Click);
             // 
             // ChkDurum
             // 
@@ -223,6 +224,7 @@
             this.CmbBrans.Name = "CmbBrans";
             this.CmbBrans.Size = new System.Drawing.Size(121, 31);
             this.CmbBrans.TabIndex = 10;
+            this.CmbBrans.SelectedIndexChanged += new System.EventHandler(this.CmbBrans_SelectedIndexChanged);
             // 
             // MskSaat
             // 
@@ -315,6 +317,7 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dataGridView1.Location = new System.Drawing.Point(3, 27);
@@ -334,6 +337,7 @@
             // 
             // dataGridView2
             // 
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dataGridView2.Location = new System.Drawing.Point(3, 27);
--- Proje_Hastane/FrmSekreterDetay.cs
+++ Proje_Hastane/FrmSekreterDetay.cs
@@ -41,6 +41,47 @@ namespace Proje_Hastane
             SqlDataAdapter da = new SqlDataAdapter("Select Bransad from Tbl_Branslar", bgl.baglanti());
             da.Fill(dt1);
             dataGridView1.DataSource = dt1;
+
+            // doktorları listeye aktarma 
+            DataTable dt2 = new DataTable();
+            SqlDataAdapter da2 = new SqlDataAdapter("Select (DoktorAd+' '+DoktorSoyad) as 'Ad Soyad',DoktorBrans From Tbl_Doktorlar", bgl.baglanti());
+            da2.Fill(dt2);
+            dataGridView2.DataSource = dt2;
+
+            // branşı combobox'a aktarma
+            SqlCommand komut2 = new SqlCommand("Select BransAd From Tbl_Branslar", bgl.baglanti());
+            SqlDataReader dr2 = komut2.ExecuteReader();
+            while (dr2.Read())
+            {
+                CmbBrans.Items.Add(dr2[0]);
+            }
+            bgl.baglanti().Close();
+        }
+
+        private void BtnKaydet_Click(object sender, EventArgs e)
+        {
+            SqlCommand komutkaydet = new SqlCommand("insert into Tbl_Randevular (RandevuTarih,RandevuSaat,RandevuBrans,RandevuDoktor) values (@r1, @r2, @r3, @r4)", bgl.baglanti());
+            komutkaydet.Parameters.AddWithValue("@r1", MskTarih.Text);
+            komutkaydet.Parameters.AddWithValue("@r2", MskSaat.Text);
+            komutkaydet.Parameters.AddWithValue("@r3", CmbBrans.Text);
+            komutkaydet.Parameters.AddWithValue("@r4", CmbDoktor.Text);
+            komutkaydet.ExecuteNonQuery();
+            bgl.baglanti().Close();
+            MessageBox.Show("Randevu Oluşturuldu!");
+        }
+
+        private void CmbBrans_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CmbDoktor.Items.Clear();
+
+            SqlCommand komut = new SqlCommand("Select DoktorAd, DoktorSoyad From Tbl_Doktorlar Where DoktorBrans=@p1", bgl.baglanti());
+            komut.Parameters.AddWithValue("@p1", CmbBrans.Text);
+            SqlDataReader dr = komut.ExecuteReader();
+            while (dr.Read())
+            {
+                CmbDoktor.Items.Add(dr[0] + " " + dr[1]);
+            }
+            bgl.baglanti().Close();
         }
     }
 }
